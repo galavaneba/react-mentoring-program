7ffad073282fdb428cde450a00f435436b317ee0
Katsiaryna Laurynovich <kaciallaurynovich@gmail.com>
2019-01-31 18:26:53 +0100

    render default movies list

--- public/javascripts/app.jsx
+++ public/javascripts/app.jsx
@@ -5,49 +5,9 @@ import axios from 'axios';
 import SearchBar from './search/SearchBar';
 import Header from "./header/Header";
 import SortBar from './sortBar/SortBar';
-import MovieCard from './movieCard/MovieCard';
-import Movie from './moviePage/Movie';
 import Footer from './footer/Footer';
 import '../stylesheets/style.less';
 import MovieList from "./movieList/MovieList";
-
-// class App extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-//
-// 		this.state = {
-// 			movies: [],
-// 		};
-// 	}
-//
-// 	componentWillMount() {
-// 		this.searchMovies();
-// 	}
-//
-// 	async searchMovies() {
-// 		 const resp = await axios.get('http://react-cdp-api.herokuapp.com/movies', {
-// 			params: {
-// 				searchBy: 'genre'
-// 			}
-// 		});
-//
-// 		 console.log(resp);
-// 		this.setState({movies: resp.data.data});
-// 	}
-//
-// 	render() {
-// 		return (
-// 			<div>
-// 				<div className="header-wrapper">
-// 					<Header />
-// 					<Movie />
-// 				</div>
-//
-// 				<Footer />
-// 			</div>
-// 		);
-// 	}
-// }
 
 class App extends React.Component {
 	constructor(props) {
@@ -58,7 +18,11 @@ class App extends React.Component {
 		};
 	}
 
-	async onSearchSubmit(term) {
+	componentDidMount() {
+		this.onSearchSubmit('comedy');
+	}
+
+	 onSearchSubmit = async term => {
 		const response = await axios.get('http://react-cdp-api.herokuapp.com/movies', {
 			params: {
 				search: term,
@@ -67,7 +31,7 @@ class App extends React.Component {
 		});
 
 		this.setState({movies: response.data.data});
-	}
+	};
 
 	render() {
 		return (
